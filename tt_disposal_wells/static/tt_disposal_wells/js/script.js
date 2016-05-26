d2129e4298894daef3e70a91a05fa8042252356a
Ryan Murphy <rdmurphy@users.noreply.github.com>
2016-05-25 23:26:27 -0500

    Fix HTTPS errors from Mapbox, removed broken Mapquest geocoder

--- tt_disposal_wells/static/tt_disposal_wells/js/script.js
+++ tt_disposal_wells/static/tt_disposal_wells/js/script.js
@@ -12,7 +12,7 @@ function getParamByName(name) {
     return decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
-var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
+// var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
 var svg = !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect;
 
 // end Helpers
@@ -29,15 +29,15 @@ var $footer = $('#control-nav');
 var $popup = $('#popup');
 var $well_id = $('#well-id');
 var $find_me = $('#find-me');
-var $geo_input = $('#geo-input');
-var $geo_fire = $('#geo-fire');
+// var $geo_input = $('#geo-input');
+// var $geo_fire = $('#geo-fire');
 
 if (!navigator.geolocation) {
     $find_me.hide();
 }
 
 if(!svg) {
-    $header.find('img').attr('src', 'http://s3.amazonaws.com/static.texastribune.org/common/tt_disposal_wells/images/txtrib-logo-white-271x30px.png');
+    $header.find('img').attr('src', 'https://s3.amazonaws.com/static.texastribune.org/common/tt_disposal_wells/images/txtrib-logo-white-271x30px.png');
 }
 
 map_settings = {  // base settings optimized for mobile
@@ -92,19 +92,19 @@ $find_me.on('click', function() {
     });
 });
 
-$('#geo-locate').on('submit', function(e) {
-    geocode($geo_input.val());
-    e.preventDefault();
-});
+// $('#geo-locate').on('submit', function(e) {
+//     geocode($geo_input.val());
+//     e.preventDefault();
+// });
 
-$geo_fire.on('click', function() {
-    var val = $geo_input.val();
-    if (!val) {
-        return false;
-    }
-
-    geocode(val);
-});
+// $geo_fire.on('click', function() {
+//     var val = $geo_input.val();
+//     if (!val) {
+//         return false;
+//     }
+//
+//     geocode(val);
+// });
 
 $('#about-button').on('click', function() {
     $modal_body = $('.about-body');
@@ -113,39 +113,39 @@ $('#about-button').on('click', function() {
     }
 });
 
-if(iOS) {
-    $geo_input.on('blur', function(e) {
-        setTimeout(function() {
-            $('body').css('height', '+=1').css('height', '-=1');
-        }, 0);
-    });
-}
+// if(iOS) {
+//     $geo_input.on('blur', function(e) {
+//         setTimeout(function() {
+//             $('body').css('height', '+=1').css('height', '-=1');
+//         }, 0);
+//     });
+// }
 
-function geocode(request) {
-  $.ajax({
-    url: 'http://open.mapquestapi.com/nominatim/v1/search?format=json&countrycodes=us&limit=1&addressdetails=1&q=' + request,
-    cache: false,
-    dataType: 'jsonp',
-    jsonp: 'json_callback',
-    success: function(response) {
-      result = response[0];
-      if (result === undefined) {
-         alert('A location could not be found. Please try searching for a ZIP Code.');
-         return false;
-      } else {
-        var lat = result.lat,
-            lng = result.lon,
-            state = result.address.state;
-        if (state !== 'Texas') {
-          alert('The address that returned is not in Texas. Please try making your query more detailed.');
-        }
-
-        var loc = L.latLng(lat, lng);
-        checkGeoAgainstGrid(loc);
-      }
-    }
-  });
-}
+// function geocode(request) {
+//   $.ajax({
+//     url: 'https://open.mapquestapi.com/nominatim/v1/search?format=json&countrycodes=us&limit=1&addressdetails=1&q=' + request,
+//     cache: false,
+//     dataType: 'jsonp',
+//     jsonp: 'json_callback',
+//     success: function(response) {
+//       result = response[0];
+//       if (result === undefined) {
+//          alert('A location could not be found. Please try searching for a ZIP Code.');
+//          return false;
+//       } else {
+//         var lat = result.lat,
+//             lng = result.lon,
+//             state = result.address.state;
+//         if (state !== 'Texas') {
+//           alert('The address that returned is not in Texas. Please try making your query more detailed.');
+//         }
+//
+//         var loc = L.latLng(lat, lng);
+//         checkGeoAgainstGrid(loc);
+//       }
+//     }
+//   });
+// }
 
 function checkGeoAgainstGrid(loc) {
     map.setView(loc, 10);
